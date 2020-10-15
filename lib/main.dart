5320725bf98a7cb28dd5d09fb626fe876512c024
Gonzalo Caira <gc98wootz@gmail.com>
2020-10-15 15:34:43 -0500

    Added ARCore

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() {
   runApp(MyApp());
@@ -14,9 +16,47 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Text('Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ArCoreController arCoreController;
+  _onArCoreViewCreated(ArCoreController _arcoreController){
+    arCoreController = _arcoreController;
+    _addSphere(arCoreController);
+  }
+  _addSphere(ArCoreController _arcoreController){
+    final material = ArCoreMaterial(color: Colors.deepOrange);
+    final sphere = ArCoreSphere(materials: [material],radius:0.2);
+    final node = ArCoreNode(shape : sphere, position : vector.Vector3(0,0,-1),);
+
+    _arcoreController.addArCoreNode(node);
+  }
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ArCoreView(onArCoreViewCreated: _onArCoreViewCreated),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
 
